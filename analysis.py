80df05a939606baaad60f1f6043448754c41e198
satellitex <s.a.t.e.3.ths@gmail.com>
2020-04-16 15:02:36 +0900

    fix analysis

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import json
 import csv
+from introducers import introducers
 
 events_path = './data/events.json'
 txs_path = './data/txs.json'
@@ -22,26 +23,52 @@ def day_to_bonus(day):
     else:
         return 1600
 
+def is_valid_intro(introducer):
+    return introducer in introducers
+
+def add_list_num(arr, address, num):
+    if address in arr:
+        arr[address] += num
+    else:
+        arr[address] = num
+
+def get_or(arr, address):
+    if address in arr:
+        return arr[address]
+    else:
+        return 0
+
 TOTAL_ISSUE_PLM = 500000000
 TOTAL_LOCKDROP_PLM = TOTAL_ISSUE_PLM * 17/20
 total_issue_ratio = 0
 eth_exchange_rate = 156.36
 tx_issue_rate = {}
+tx_introducer = {}
+tx_issue_plm = {}
 
 for event in events['result']:
     # value
     value = int(event['topics'][1], 16)
     # day
     day = int(event['topics'][2], 16)
+    introducer = '0x' + event['data'][-40:]
+    print('introucer:',introducer)
     issue_ratio = value * day_to_bonus(day) * eth_exchange_rate
+    tx_hash = event['transactionHash']
     # print(value, day)
     total_issue_ratio += issue_ratio
-    tx_issue_rate[event['transactionHash']] = issue_ratio
+    tx_issue_rate[tx_hash] = issue_ratio
+    tx_introducer[tx_hash] = introducer
+
 
 print(total_issue_ratio)
 
 address_list = []
 plm_list = {}
+plm_ref_list = {}
+plm_intro_list = {}
+refrenced_bonus = {}
+introduce_bonus = {}
 
 for tx in txs['result']:
     hash = tx['hash']
@@ -49,11 +76,16 @@ for tx in txs['result']:
     if not hash in tx_issue_rate:
         continue
     issued_plm = TOTAL_LOCKDROP_PLM * tx_issue_rate[hash] / total_issue_ratio
-    if address in plm_list:
-        plm_list[address] += issued_plm
-    else:
-        plm_list[address] = issued_plm
-    
+    tx_issue_plm[hash] = issued_plm
+
+    add_list_num(plm_list, address, issued_plm)
+
+    introducer = tx_introducer[hash]
+    if is_valid_intro(introducer):
+        print('introduce!',introducer)
+        issued_aff_plm = issued_plm / 100
+        add_list_num(plm_ref_list, introducer, issued_aff_plm)
+        add_list_num(plm_intro_list, address, issued_aff_plm)        
 
 
 for (address, plm) in plm_list.items():
@@ -70,5 +102,7 @@ print('alpha_3', alpha_3)
 
 with open('data/out.csv', 'w') as f:
     writer = csv.writer(f)
+    writer.writerow(['address', 'normal', 'ref_bonnus', 'intro_bonnus', 'all'])
     for (address, plm) in plm_list.items():
-        writer.writerow([address, plm])
+        all_plm = plm + get_or(plm_ref_list, address) + get_or(plm_intro_list, address)
+        writer.writerow([address, plm, get_or(plm_ref_list, address), get_or(plm_intro_list, address), all_plm])
